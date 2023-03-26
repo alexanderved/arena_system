bae71735f5300af2a256a851cdc521617efa8fe7
alexanderved <avedehin692@gmail.com>
2023-03-26 20:29:52 +0300

    Update Index

--- src/index.rs
+++ src/index.rs
@@ -2,25 +2,33 @@ use std::convert;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Index {
-    index: usize,
+    index: i64,
 }
 
 impl Index {
-    pub fn new(index: usize) -> Self {
+    pub fn new(index: i64) -> Self {
         Self {
             index
         }
+    }
+
+    pub fn invalid() -> Self {
+        Self::new(-1)
     }
 }
 
 impl convert::Into<usize> for Index {
     fn into(self) -> usize {
-        self.index
+        if self.index < 0 {
+            panic!("Trying to convert invalid Index to usize");
+        }
+
+        self.index as usize
     }
 }
 
 impl convert::From<usize> for Index {
     fn from(index: usize) -> Self {
-        Self::new(index)
+        Self::new(index as i64)
     }
 }
